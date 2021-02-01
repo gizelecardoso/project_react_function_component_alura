b64ef81cd8e06272f1db0ecc46b516aabb7e297d
Gizele Santos <gizele.santos@repassa.com.br>
2021-02-01 14:16:44 -0300

    Finalizando curso - adicionando Hooks, validaçoes e erros, mais sobre a lib Material UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 import {Container, Typography} from "@material-ui/core";
@@ -11,9 +11,21 @@ class App extends Component {
         <Typography variant="h3" component="h1" align="center">
           Formulario Cadastro
         </Typography>
-        <FormularioCadastro />
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF}/>
       </Container>
       );
+  }
+}
+
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto: "CPF deve ter 11 digitos"}
+  } else {
+    return {valido:true, texto: ""}
   }
 }
 
